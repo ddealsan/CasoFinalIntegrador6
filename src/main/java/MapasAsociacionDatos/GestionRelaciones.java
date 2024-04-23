667e2c63ed8e5b94acad846142f9926e21bd8f01
ddealsan <ddealsan@myuax.com>
2024-04-23 12:07:11 +0200

    avance

--- src/main/java/MapasAsociacionDatos/GestionRelaciones.java
+++ src/main/java/MapasAsociacionDatos/GestionRelaciones.java
@@ -1,4 +1,17 @@
 package MapasAsociacionDatos;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GestionRelaciones {
+
+    private Map<Integer, String> relaciones = new HashMap<>();
+
+    public void agregarRelacion(int numero, String texto) {
+        relaciones.put(numero, texto);
+    }
+
+    public String obtenerTexto(int numero) {
+        return relaciones.get(numero);
+    }
 }
