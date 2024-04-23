cc1d1e1fed92d08a7b531a065815ee7aa8f5e957
ddealsan <ddealsan@myuax.com>
2024-04-23 12:07:57 +0200

    avance

--- src/main/java/IndexacionVisualizacionArchivos/OrdenacionListado.java
+++ src/main/java/IndexacionVisualizacionArchivos/OrdenacionListado.java
@@ -1,4 +1,21 @@
 package IndexacionVisualizacionArchivos;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class OrdenacionListado {
+
+    private IndexacionRecursiva indexacionRecursiva;
+
+    public OrdenacionListado(IndexacionRecursiva indexacionRecursiva) {
+        this.indexacionRecursiva = indexacionRecursiva;
+    }
+
+    public void listarArchivosOrdenados() {
+        Map<String, String> indexacionOrdenada = new TreeMap<>(indexacionRecursiva.getIndexacion());
+
+        for (Map.Entry<String, String> entrada : indexacionOrdenada.entrySet()) {
+            System.out.println("Nombre del archivo: " + entrada.getKey() + ", Ruta completa: " + entrada.getValue());
+        }
+    }
 }
--- src/main/java/MapasAsociacionDatos/RecuperacionEficiente.java
+++ src/main/java/MapasAsociacionDatos/RecuperacionEficiente.java
@@ -1,4 +1,14 @@
 package MapasAsociacionDatos;
 
 public class RecuperacionEficiente {
+
+    private GestionRelaciones gestionRelaciones;
+
+    public RecuperacionEficiente(GestionRelaciones gestionRelaciones) {
+        this.gestionRelaciones = gestionRelaciones;
+    }
+
+    public String recuperarTexto(int numero) {
+        return gestionRelaciones.obtenerTexto(numero);
+    }
 }
