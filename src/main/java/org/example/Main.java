7a28c0f70c8bcb4fc512b4193617e08b0cd8d5f2
ddealsan <ddealsan@myuax.com>
2024-04-23 10:05:24 +0200

    avance

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,7 +16,7 @@ public class Main {
 class Bienvenida extends JFrame {
     public Bienvenida() {
         setTitle("Bienvenido");
-        setSize(600, 400);
+        setSize(1000, 600); // Aumentar el ancho de la ventana
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         getContentPane().setBackground(Color.WHITE);
@@ -30,7 +30,7 @@ class Bienvenida extends JFrame {
         // Cargar imagen
         ImageIcon imageIcon = new ImageIcon(getClass().getResource("/FotoLogo.png"));
         Image image = imageIcon.getImage();
-        Image scaledImage = image.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+        Image scaledImage = image.getScaledInstance(450, 300, Image.SCALE_SMOOTH); // Ajustar el tamaño de la imagen a 350x350
         imageIcon = new ImageIcon(scaledImage);
         JLabel imageLabel = new JLabel(imageIcon);
         leftPanel.add(imageLabel, BorderLayout.NORTH);
@@ -38,7 +38,7 @@ class Bienvenida extends JFrame {
         // Texto de bienvenida
         JLabel welcomeLabel = new JLabel("<html><div style='text-align: center;'>Bienvenido al Sistema de Gestión y Análisis de Datos Multidimensionales</div></html>", SwingConstants.CENTER);
         welcomeLabel.setForeground(Color.BLUE);
-        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 30)); // Aumentar el tamaño de la fuente
         leftPanel.add(welcomeLabel, BorderLayout.CENTER);
 
         // Panel derecho
@@ -50,6 +50,7 @@ class Bienvenida extends JFrame {
         JButton button1 = new JButton("Gestión de datos dinámicos");
         button1.setForeground(Color.WHITE);
         button1.setBackground(Color.BLUE);
+        button1.setFont(new Font("Arial", Font.BOLD, 20)); // Aumentar el tamaño de la fuente
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -62,6 +63,7 @@ class Bienvenida extends JFrame {
         JButton button2 = new JButton("Análisis y organización de información");
         button2.setForeground(Color.WHITE);
         button2.setBackground(Color.BLUE);
+        button2.setFont(new Font("Arial", Font.BOLD, 20)); // Aumentar el tamaño de la fuente
         button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
