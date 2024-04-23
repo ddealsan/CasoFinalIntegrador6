a7e09518a842e9c6aadc7a1727e84fbd008b0ce8
ddealsan <ddealsan@myuax.com>
2024-04-23 22:08:57 +0200

    avance

---
+++ src/main/java/MapasAsociacionDatos/VentanaMapasAsociacion.java
@@ -0,0 +1,13 @@
+package MapasAsociacionDatos;
+
+import javax.swing.*;
+
+public class VentanaMapasAsociacion extends JFrame {
+    public VentanaMapasAsociacion() {
+        setTitle("Mapas y Asociación de Datos");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+}
