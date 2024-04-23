f4f885f93f59e6f2a3d23288953a9c3e95804b7f
ddealsan <ddealsan@myuax.com>
2024-04-23 12:07:29 +0200

    avance

--- src/main/java/IndexacionVisualizacionArchivos/IndexacionRecursiva.java
+++ src/main/java/IndexacionVisualizacionArchivos/IndexacionRecursiva.java
@@ -1,4 +1,28 @@
 package IndexacionVisualizacionArchivos;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 public class IndexacionRecursiva {
+
+    private Map<String, String> indexacion = new HashMap<>();
+
+    public void indexarDirectorio(File directorio) {
+        for (File file : directorio.listFiles()) {
+            if (file.isDirectory()) {
+                indexarDirectorio(file);
+            } else {
+                indexacion.put(file.getName(), file.getAbsolutePath());
+            }
+        }
+    }
+
+    public String obtenerRuta(String nombreArchivo) {
+        return indexacion.get(nombreArchivo);
+    }
+
+    public Map<String, String> getIndexacion() {
+        return indexacion;
+    }
 }
