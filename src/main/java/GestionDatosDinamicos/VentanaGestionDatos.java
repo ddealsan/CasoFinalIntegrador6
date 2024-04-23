b8f6e6b2f9388bf12996f56a763472246f3f2b45
ddealsan <ddealsan@myuax.com>
2024-04-23 22:08:45 +0200

    avance

---
+++ src/main/java/GestionDatosDinamicos/VentanaGestionDatos.java
@@ -0,0 +1,13 @@
+package GestionDatosDinamicos;
+
+import javax.swing.*;
+
+public class VentanaGestionDatos extends JFrame {
+    public VentanaGestionDatos() {
+        setTitle("Gestión de Datos Dinámicos");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // Aquí puedes agregar los componentes de la ventana
+    }
+}
