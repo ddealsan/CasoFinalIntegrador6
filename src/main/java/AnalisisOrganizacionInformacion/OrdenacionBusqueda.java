74f863e1e2936c9dcf6833c5d85a7cb7865b7591
ddealsan <ddealsan@myuax.com>
2024-04-23 12:07:47 +0200

    avance

--- src/main/java/AnalisisOrganizacionInformacion/OrdenacionBusqueda.java
+++ src/main/java/AnalisisOrganizacionInformacion/OrdenacionBusqueda.java
@@ -1,4 +1,16 @@
 package AnalisisOrganizacionInformacion;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class OrdenacionBusqueda {
+
+    public static void ordenarNombres(List<String> nombres) {
+        Collections.sort(nombres);
+    }
+
+    public static void ordenarVentas(List<Venta> ventas, Comparator<Venta> comparator) {
+        ventas.sort(comparator);
+    }
 }
