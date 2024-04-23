a8e771e7ac340a7ca529cc1742a5187554c9ca65
ddealsan <ddealsan@myuax.com>
2024-04-23 12:06:54 +0200

    avance

--- src/main/java/AnalisisOrganizacionInformacion/AnalisisRegistros.java
+++ src/main/java/AnalisisOrganizacionInformacion/AnalisisRegistros.java
@@ -1,4 +1,13 @@
 package AnalisisOrganizacionInformacion;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AnalisisRegistros {
+
+    public static List<Venta> filtrarVentas(List<Venta> ventas, String producto) {
+        return ventas.stream()
+                .filter(venta -> venta.getProducto().equals(producto))
+                .collect(Collectors.toList());
+    }
 }
