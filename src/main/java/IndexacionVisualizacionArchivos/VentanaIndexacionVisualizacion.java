018904fc9256a9ffea70991bd1c24ccbe77eaf6d
ddealsan <ddealsan@myuax.com>
2024-04-23 22:08:52 +0200

    avance

---
+++ src/main/java/IndexacionVisualizacionArchivos/VentanaIndexacionVisualizacion.java
@@ -0,0 +1,13 @@
+package IndexacionVisualizacionArchivos;
+
+import javax.swing.*;
+
+public class VentanaIndexacionVisualizacion extends JFrame {
+    public VentanaIndexacionVisualizacion() {
+        setTitle("Indexación y Visualización de Archivos");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // Aquí puedes agregar los componentes de la ventana
+    }
+}
